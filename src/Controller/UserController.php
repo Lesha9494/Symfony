66d2d146d1a65860911614777d090e23d8b7b85f
Lesha <glus543hkov@gmail.com>
2025-03-11 13:19:57 +0300

    Готовое задание

---
+++ src/Controller/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em) {}
+
+    #[Route('/user', name: 'index_user')]
+    public function index(Request $request, UserRepository $userRepository): Response
+    {
+        $query = $request->query->get('q');
+
+        $qb = $userRepository->createQueryBuilder('u');
+        if ($query) {
+            $qb->andWhere('u.lastName LIKE :query OR u.firstName LIKE :query OR u.email LIKE :query')
+                ->setParameter('query', '%' . $query . '%');
+        }
+
+        $users = $qb->getQuery()->getResult();
+
+        return $this->render('/user/index.html.twig', ['users' => $users]);
+    }
+
+    #[Route('/user/create', name: 'create_user', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $user = new User();
+            $user->setLastName($request->request->get('last_name'));
+            $user->setFirstName($request->request->get('first_name'));
+            $user->setAge($request->request->get('age'));
+            $user->setStatus($request->request->get('status'));
+            $user->setEmail($request->request->get('email'));
+            $user->setTelegram($request->request->get('telegram'));
+            $user->setAddress($request->request->get('address'));
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $this->redirectToRoute('index_user');
+        }
+
+        return $this->render('user/create.html.twig');
+    }
+
+    #[Route('/user/{user}', name: 'edit_user', methods: ['GET'])]
+    public function edit(User $user): Response
+    {
+        return $this->render('user/edit.html.twig', ['user' => $user]);
+    }
+
+    #[Route('/user/{user}', name: 'update_user', methods: ['POST', 'PUT'])]
+    public function update(User $user, Request $request): Response
+    {
+        $user->setLastName($request->request->get('last_name', $user->getLastName()));
+        $user->setFirstName($request->request->get('first_name', $user->getFirstName()));
+        $user->setAge($request->request->get('age', $user->getAge()));
+        $user->setStatus($request->request->get('status', $user->getStatus()));
+        $user->setEmail($request->request->get('email', $user->getEmail()));
+        $user->setTelegram($request->request->get('telegram', $user->getTelegram()));
+        $user->setAddress($request->request->get('address', $user->getAddress()));
+
+        $this->em->flush();
+
+        return $this->redirectToRoute('index_user');
+    }
+
+    #[Route('/user/{user}', name: 'delete_user', methods: ['POST'])]
+    public function delete(User $user, EntityManagerInterface $em): Response
+    {
+        $em->remove($user);
+        $em->flush();
+    
+        return $this->redirectToRoute('index_user');
+    }
+    
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column]
+    private ?int $age = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telegram = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): static
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelegram(): ?string
+    {
+        return $this->telegram;
+    }
+
+    public function setTelegram(string $telegram): static
+    {
+        $this->telegram = $telegram;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+}
